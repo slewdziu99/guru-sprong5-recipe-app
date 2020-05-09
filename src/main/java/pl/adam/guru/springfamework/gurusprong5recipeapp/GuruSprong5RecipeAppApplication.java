f7058d6d86c37f5e84a6071ce5705b3fa59602a4
sledziu99 <adam.slazinski@gmail.com>
2020-05-09 22:46:44 +0200

    Szblon aplikacji

---
+++ src/main/java/pl/adam/guru/springfamework/gurusprong5recipeapp/GuruSprong5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package pl.adam.guru.springfamework.gurusprong5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSprong5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruSprong5RecipeAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/adam/guru/springfamework/gurusprong5recipeapp/GuruSprong5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adam.guru.springfamework.gurusprong5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruSprong5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
